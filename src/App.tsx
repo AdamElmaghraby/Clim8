2b7d2904a42d4cdda09076a662160af8fa2838fe
Adam Elmaghraby <adamfingersen@gmail.com>
2025-04-15 21:40:59 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      Hi
+      <Button variant="destructive">Subscribe</Button>
+    </div>
+  );
+}
+
+export default App;
