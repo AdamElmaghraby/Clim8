b27762bfd2f67fd6ea5393b2b05cf4a75d606ae3
Adam Elmaghraby <adamfingersen@gmail.com>
2025-04-18 22:34:02 -0500

    handling api config

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,30 @@ import Layout from "./components/layout";
 import { ThemeProvider } from "./context/theme-provider";
 import WeatherDashboard from "./pages/weather-dashboard";
 import CityPage from "./pages/city-page";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="system">
-        <Layout> 
-          <Routes>
-            <Route path='/' element={<WeatherDashboard/>}> </Route>
-            <Route path='/city/:cityName' element={<CityPage/>}> </Route>
-          </Routes> 
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="system">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />}>
+                {" "}
+              </Route>
+              <Route path="/city/:cityName" element={<CityPage />}>
+                {" "}
+              </Route>
+            </Routes>
           </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
