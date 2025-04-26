571a7dad4bbc03cd77289e847c7d2765a9247a90
Adam Elmaghraby <adamfingersen@gmail.com>
2025-04-26 00:52:28 -0500

    added alert to getLocation on location error

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,7 +1,8 @@
 import WeatherSkeleton from "@/components/loading-skeleton";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { useGeolocation } from "@/hooks/use-geolocation";
-import { RefreshCcw } from "lucide-react";
+import { AlertCircle, MapPin, RefreshCcw } from "lucide-react";
 import React from "react";
 
 const WeatherDashboard = () => {
@@ -21,7 +22,23 @@ const WeatherDashboard = () => {
   };
 
   if (locationLoading) {
-    return WeatherSkeleton;
+    return <WeatherSkeleton />;
+  }
+
+  if (locationError) {
+    return (
+      <Alert variant="destructive" className="bg-transparent">
+        <AlertCircle className="h-4 w-4" />
+        <AlertTitle>Location Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>{locationError}</p>
+          <Button onClick={getLocation} variant="destructive" className="w-fit">
+            <MapPin className="mr-2 h-4 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
   }
 
   return (
